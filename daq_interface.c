c6ae1728c7a92674df836679300d0a6663de33d1
chinmaybhale <chinmay.bhale@live.com>
2020-05-17 18:41:41 -0400

    Added files for interfacing with DAQ

---
+++ daq_interface.c
@@ -0,0 +1,25 @@
+// HELIOS ROCKETRY
+// This file contains the code for interfacing with the daq
+#include "headers.h"
+
+static void convert()
+{
+	// TODO: convert values from 
+	// Volts -> PSI/C/other format
+	
+	return;
+}
+
+static void read_data()
+{
+	// TODO: read code from DAQ here
+	
+	return;
+}
+
+void get_data()
+{
+	read_data();
+	convert();
+	return;
+}
---
+++ daq_interface.h
@@ -0,0 +1,6 @@
+#ifndef __DAQ_INTERFACE_H__
+#define __DAQ_INTERFACE_H__
+
+void get_data();
+
+#endif
