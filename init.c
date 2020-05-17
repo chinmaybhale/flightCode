18bb11ec4fcbf0dc6652fa73cb27d0f74e218370
chinmaybhale <chinmay.bhale@live.com>
2020-05-17 18:40:36 -0400

    Added declaration of function for init sensors and valves

--- init.c
+++ init.c
@@ -4,8 +4,33 @@
 
 #include "headers.h"
 
-static void read_config()
+static void read_config(char *name)
 {
+	// CSV FORMAT
+	// sensor_name, base_val, +ve error, -ve error, pin# (if availabe, -1 otherwise)
+	// or
+	// valve_name, pin#
+	//
+	// sensor_name will decide if it is a pressure or temperature or other kind of sensor.
+
+	return;
+}
+
+static void init_sensor(char *setup)
+{
+	// TODO: split the string along the comma
+	// and convert the values to float
+	// use stof() for this.
+	
+	return;
+}
+
+static void init_valve(char *setup)
+{
+	// TODO: convert the pin number to int
+	// using stoi() and assign to valve
+	// set open to 0 (off)	
+	
 	return;
 }
 
@@ -14,9 +39,9 @@ static void system_check()
 	return;
 }
 
-int init()
+void init(char *name)
 {
-	read_config();
+	read_config(name);
 	system_check();
 	return 0;
 }
--- init.h
+++ init.h
@@ -1,6 +1,6 @@
 #ifndef __INIT_H__
 #define __INIT_H__
 // global function declarations go here
-int init();
+void init();
 
 #endif
