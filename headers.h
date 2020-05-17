b64a3d413bab5fdeee3a49099e51d7a8295f5c5b
chinmaybhale <chinmay.bhale@live.com>
2020-05-17 18:39:39 -0400

    Redefined sensor struct and added daq_interface

--- headers.h
+++ headers.h
@@ -8,24 +8,36 @@
 #include "macros.h"
 #include "init.h"
 #include "seq.h"
+#include "daq_interface.h"
 
 // Global variables
 // used for storing values of pressure, temp, etc
 // declared here, initialized in init
 
 // max and min values decided by percentage when initializing
-struct sensors {
+struct sensor {
 	float max_val;
 	float min_val;
-	short pin;
+	short pin; // not all sensors will have this, set to -1
 };
 
-struct sensors p[8]; // for pressure values
-struct sensors t[5]; // for temperature values
+struct valve {
+	short pin;
+	short stat; // 0 = off, 1 = on
+};
 
-typedef short valves;
+struct daq_value {
+	// TODO:
+	// we need a specific data structure to store data from DAQ
+	// let's define this once we get a better idea with what
+	// we are dealing with.
+};
 
-valves main_v[7];
-valves backup_v[5];
+
+struct sensor p[8]; // for pressure values
+struct sensor t[5]; // for temperature values
+
+struct valve main_v[7];
+struct valve backup_v[5];
 
 #endif
