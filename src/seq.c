b3483ed2a7df6c9d8b0b1c780f9e486a16b2f4c8
chinmaybhale <chinmay.bhale@live.com>
2020-06-05 16:39:20 -0400

    Added update function to calculate new sensor values

--- src/seq.c
+++ src/seq.c
@@ -1,5 +1,23 @@
 #include "../include/headers.h"
 
+static void update()
+{
+	/**
+	 * this function updates the sensor values
+	 * at the start of each iteration. daq values
+	 * are checked against the updated values.
+	 * 
+	 * Args:
+	 * 	None
+	 * 
+	 * Returns:
+	 * 	int: 0 
+	 * 
+	**/
+	
+	return;
+}
+
 int seq()
 {
 	/**
