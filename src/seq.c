f71e47f8657716c9af68af6977c2f4d98d8d8c27
chinmaybhale <chinmay.bhale@live.com>
2020-06-27 10:05:23 -0400

    Removed verified-file function

--- src/seq.c
+++ src/seq.c
@@ -16,13 +16,9 @@
 #include "../include/headers.h"
 //---------------------------------------------------------------------------------------------------------------------------
 
-FILE *verified_file;
 FILE *verified_val_file;
 char *verified_val_line;
 char *line;
-
-
-static int read_verified();
 
 int seq()
 {
@@ -227,66 +223,6 @@ int seq()
 }
 //---------------------------------------------------------------------------------------------------------------------------
 
-static int read_verified()
-{
-	/**
-	 * this function is called whenever you want to get the next line of the verified/expected
-	 * values for the sensors
-	 */
-	char str_val[30];
-	int len = 256;
-
-	if (fgets(line, len, verified_file) == NULL)
-	{
-		printf("end of verified file");
-		free(line);
-		fclose(verified_file);
-		return 0;
-	}
-	else
-	{
-
-		int curr, prev = 0, i = 0;
-
-		for (curr = 0; curr <= len; curr++)
-		{
-			if (line[curr] == ',' || line[curr] == '\n')
-			{
-				strncpy(str_val, line + prev, (curr - prev));
-				str_val[curr - prev] = '\0';
-				verified_data[i++].base_val = atof(str_val);
-				prev = curr + 1;
-			}
-			if (line[curr] == '\n')
-				break;
-		}
-	}
-
-	return 1;
-}
-//---------------------------------------------------------------------------------------------------------------------------
-
-void init_verified_file()
-{
-	/**
-	 * this function is used to initiate the process of storing 
-	 * the verified/expected values for our sensors.
-	 */
-
-	verified_file = fopen("verifiedFile.csv", "r");
-
-	if (!verified_file)
-		printf("unable to open verifiedFile.csv");
-
-	line = (char *)malloc(sizeof(char) * 256);
-
-	if (fgets(line, 256, verified_file) == NULL)
-		printf("no data in verified file");
-
-	return;
-}
-//---------------------------------------------------------------------------------------------------------------------------
-
 static int read_verified_value()
 {
 	/**
@@ -331,8 +267,9 @@ static int read_verified_value()
 				case 3 :
 					//the final value we encounter is the neg err, then we reset count to 1
 					s[i].neg_val_err = atoi(str_val);
-					s[i++].min_val = s[i].base_val - (s[i].base_val * s[i].neg_val_err);
+					s[i].min_val = s[i].base_val - (s[i].base_val * s[i].neg_val_err);
 					count = 1;
+					i++;
 					break;
 				
 				default :
